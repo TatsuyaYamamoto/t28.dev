c032884c069ec101a508e118a1ac64dfcdf6d974
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2021-04-08 00:07:16 +0900

    chore: add ref link

--- src/types/css.d.ts
+++ src/types/css.d.ts
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/41336858/how-to-import-css-modules-with-typescript-react-and-webpack
 declare module '*.css' {
   interface IClassNames {
     [className: string]: string
