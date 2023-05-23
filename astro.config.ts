765dd148b5c40110cd1f9131de1168e8fa8d1e81
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2023-05-23 11:03:50 +0900

    chore: add react integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
+  integrations: [react()],
   vite: {
     resolve: {
       // https://www.eliostruyf.com/symlink-content-astro-portability/
