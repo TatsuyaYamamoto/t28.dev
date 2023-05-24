835f6135c5b2273a260d9288c4de3e66e3f4fca5
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2023-05-24 10:43:18 +0900

    chore: add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [react()],
+  site: "https://t28.dev",
+  integrations: [react(), sitemap()],
   vite: {
     resolve: {
       // https://www.eliostruyf.com/symlink-content-astro-portability/
