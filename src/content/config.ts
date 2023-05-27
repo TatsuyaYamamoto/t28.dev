1835317042f8d2950b6bff92c5081b98efa54678
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2023-05-27 12:45:12 +0900

    chore: add achievement content collecti on scheme

--- src/content/config.ts
+++ src/content/config.ts
@@ -20,7 +20,33 @@ const roundup = defineCollection({
   }),
 });
 
+const sWorksAchievement = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    description: z.string(),
+    project: z.union([
+      z.literal("ラブライブ！シリーズ"),
+      z.literal("ラブライブ！虹ヶ咲学園スクールアイドル同好会"),
+      z.literal("こどものままでもママになりたい！"),
+    ]),
+    heroImage: z.string(),
+    links: z
+      .object({
+        label: z.string(),
+        href: z.string(),
+        type: z.union([z.literal("twitter"), z.literal("external")]),
+      })
+      .array(),
+  }),
+});
+
 export const collections = {
   blog,
   roundup,
+  ["s-works-achievement"]: sWorksAchievement,
 };
