55409b2752c2eb82bceec75e466f29ae62de3cbc
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2024-01-28 22:56:42 +0900

    chore: fix plugin

--- src/plugins/inferDescriptionMetaPlugin.ts
+++ src/plugins/inferDescriptionMetaPlugin.ts
@@ -1,9 +1,6 @@
 import rehypeInferDescriptionMeta from "rehype-infer-description-meta";
 
-import type {
-  RehypePlugin,
-  MarkdownAstroData,
-} from "@astrojs/markdown-remark/dist/types";
+import type { RehypePlugin, MarkdownAstroData } from "@astrojs/markdown-remark";
 
 export const inferDescriptionMetaPlugin: RehypePlugin = () => {
   const meta = rehypeInferDescriptionMeta() ?? null;
@@ -13,11 +10,7 @@ export const inferDescriptionMetaPlugin: RehypePlugin = () => {
       return;
     }
 
-    const dummyCallback = () => {
-      // 型上は meta(node: Root, file: VFile, next: TransformCallback) となっているが、 `next` は実装上は定義されていない
-      // https://github.com/rehypejs/rehype-infer-description-meta/blob/main/index.js#L61
-    };
-    meta(tree, file, dummyCallback);
+    meta(tree, file);
 
     (file.data.astro as MarkdownAstroData).frontmatter.excerpt =
       // rehype-infer-description-meta は結果の文字列を meta.description に上書きする
