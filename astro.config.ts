6bc4d330f72169d6fc21e96e8284aac48d6cbe3f
Tatsuya Yamamoto / T28_tatsuya <t28.is.tatsuya@gmail.com>
2024-02-07 12:36:50 +0900

    build: add mermaid plugin to render mermaid diagram (#64)
    * build: add deps
    
    * chore: add plugin
    
    chore: add plugin
    
    * pref: edit performance
    
    * chore: add md
    
    * Revert "chore: add md"
    
    This reverts commit 3f217a7a3b451e2eee3ece2ce6031be39d3af2cb.

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 import svgr from "vite-plugin-svgr";
 
 import { inferDescriptionMetaPlugin } from "./src/plugins/inferDescriptionMetaPlugin";
+import { mermaidRemarkPlugin } from "./src/plugins/mermaidRemarkPlugin.ts";
 
 export default defineConfig({
   site: "https://t28.dev",
@@ -14,6 +15,7 @@ export default defineConfig({
   integrations: [react(), mdx(), sitemap()],
   markdown: {
     rehypePlugins: [inferDescriptionMetaPlugin],
+    remarkPlugins: [mermaidRemarkPlugin],
   },
   vite: {
     plugins: [svgr()],
---
+++ src/plugins/mermaidRemarkPlugin.ts
@@ -0,0 +1,63 @@
+import type { RemarkPlugin } from "@astrojs/markdown-remark";
+import { visit } from "unist-util-visit";
+import { renderMermaid as renderMermaidCli } from "@mermaid-js/mermaid-cli";
+import puppeteer from "puppeteer";
+import type { Code, Parent, Html } from "mdast";
+
+interface MermaidCodeBlock {
+  code: Code;
+  index: number;
+  parent: Parent;
+}
+
+export const mermaidRemarkPlugin: RemarkPlugin = () => {
+  return async (root) => {
+    // find "mermaid" code blocks
+    const mermaidCodeBlocks: MermaidCodeBlock[] = [];
+    visit(root, "code", (code, index, parent) => {
+      if (code.lang === "mermaid" && index !== null && parent !== null) {
+        mermaidCodeBlocks.push({ code, index, parent });
+      }
+    });
+
+    if (mermaidCodeBlocks.length === 0) {
+      // no mermaid code blocks are found in markdown file
+      return;
+    }
+
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+
+    await Promise.all(
+      mermaidCodeBlocks.map(async ({ code, index, parent }, blockIndex) => {
+        const { data: svgBuffer } = await renderMermaidCli(
+          browser,
+          code.value,
+          "svg",
+        );
+        const svgText = svgBuffer
+          .toString("utf-8")
+          // astro(?) throws error on rendering svg of mermaid.
+          // https://github.com/withastro/astro/issues/9856
+          // set "Default implied ARIA semantics" value as workaround.
+          // https://svgwg.org/svg2-draft/struct.html#implicit-aria-semantics
+          .replace(
+            `role="graphics-document document"`,
+            `role="graphics-document"`,
+          )
+          .replaceAll(`my-svg`, `my-svg-${blockIndex}`);
+
+        // overwrite "code node" with "html node" to render svg element
+        const svgNode: Html = {
+          ...code,
+          type: "html",
+          value: `<p>${svgText}</p>`,
+        };
+        parent.children[index] = svgNode;
+      }),
+    );
+
+    await browser.close();
+  };
+};
