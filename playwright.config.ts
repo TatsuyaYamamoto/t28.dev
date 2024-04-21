59914c1b4923645fdc5192d610bfe650a39161b6
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2024-04-21 23:30:58 +0900

    chore: use token

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices, PlaywrightTestConfig } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
