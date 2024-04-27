6cc364e564b932494151c2849c03804c9522c900
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2024-04-28 02:30:15 +0900

    chore: replace index page style

--- panda.config.ts
+++ panda.config.ts
@@ -17,6 +17,28 @@ export default defineConfig({
   theme: {
     extend: {},
   },
+  patterns: {
+    extend: {
+      container: {
+        transform(props) {
+          const pandaDefault = {
+            position: "relative",
+            maxWidth: "8xl",
+            mx: "auto",
+            px: { base: "4", md: "6", lg: "8" },
+            ...props,
+          };
+
+          return {
+            ...pandaDefault,
+            maxWidth: "1200px",
+            mx: "auto",
+            px: "var(--spacing-4)",
+          };
+        },
+      },
+    },
+  },
 
   // The output directory for your css system
   outdir: "styled-system",
