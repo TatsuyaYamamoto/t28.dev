121ed046e4de268d3f2cdc12629f20a9e457cd72
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2024-04-28 17:55:11 +0900

    ci: add vrt target pages

--- tests/vrt/pages.test.ts
+++ tests/vrt/pages.test.ts
@@ -1,8 +1,20 @@
 import { test, expect } from "@playwright/test";
 
 const pages = [
-  { path: "/", name: "IndexPage" },
-  { path: "/blog/render-mermaid-on-astro-site", name: "BlogPostSample" },
+  { path: "/", name: "t28.dev top page" },
+  {
+    path: "/blog/render-mermaid-on-astro-site",
+    name: "blog post page (example)",
+  },
+  {
+    path: "/blog/render-mermaid-on-astro-site/ogp.png",
+    name: "blog post ogp image (example)",
+  },
+  { path: "/s-works", name: "s-works top page" },
+  {
+    path: "/s-works/achievement/tokimeki_message",
+    name: "s-works achievement page (example)",
+  },
 ];
 
 pages.forEach((target) => {
