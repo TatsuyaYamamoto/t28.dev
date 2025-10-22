81e6ade90edf410fea6be6824fe2de66ab781aac
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-10-22 11:51:24 +0900

    chore: add plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-astro"],
+  plugins: [
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+  ],
   overrides: [
     // https://github.com/withastro/prettier-plugin-astro?tab=readme-ov-file#recommended-configuration
     {
