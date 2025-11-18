2e1d0caa5c0881244478c2e2f92dc147e8f034cd
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-11-18 13:02:32 +0900

    chore: use tailwind directive

--- src/components/s-works/IndexHero.tsx
+++ src/components/s-works/IndexHero.tsx
@@ -6,14 +6,7 @@ const IndexHero: FC = () => {
   return (
     <div className="box-border flex min-h-full items-center justify-center border-16 border-solid border-(--color-s-works)">
       <h1 className="inline-flex items-center">
-        <SWorksLogo
-          className="animate-s-works-surface w-[350px] md:w-[600px]"
-          style={{
-            animationName: "sWorksSurface",
-            animationFillMode: "both",
-            animationDuration: "800ms",
-          }}
-        />
+        <SWorksLogo className="animate-s-works-surface w-[350px] md:w-[600px]" />
       </h1>
     </div>
   );
