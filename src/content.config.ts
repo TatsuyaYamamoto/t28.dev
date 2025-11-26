f7677217d9863816ba7f430859d0d8f00309dbc9
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-11-26 11:12:28 +0900

    chore: remove legacy api

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "*.md", base: "./src/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
@@ -14,7 +15,7 @@ const blog = defineCollection({
 });
 
 const sWorksAchievement = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "*.mdx", base: "./src/s-works-achievement" }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
